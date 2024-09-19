18183c25667d03f9ffecd27b587661830b2a2a26
princenizroh <princenizroh7@gmail.com>
2024-09-19 20:05:31 +0800

    bootstrap

---
+++ portofolio/img/prom2.py
@@ -0,0 +1,12 @@
+
+masukan = input
+
+if (masukan == "AUG"):
+    print("methionie")
+elif (masukan == "UUU" or masukan == "UUC"):
+    print("phenylanine")
+elif (masukan == "UUA" or masukan == "UUG")
+    print("leucine")
+elif (masukan == "UCU" or masukan "UCC" or masukan "UCA" or masukan "UCG"):
+    print("serine")
+    
