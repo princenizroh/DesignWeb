dccba93b698193df1e2514b12a23cbd88ef66459
princenizroh <princenizroh7@gmail.com>
2024-10-24 20:41:50 +0800

    push

---
+++ portofolio-vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <UserLogin v-if="!isLoggedIn" @loginSuccess="isLoggedIn = true" />
+    <UserDashboard v-else @logout="isLoggedIn = false" />
+  </div>
+</template>
+
+<script>
+import UserLogin from './components/UserLogin.vue'; 
+import UserDashboard from './components/UserDashboard.vue'; 
+export default {
+  name: 'App',
+  components: {
+    UserLogin,
+    UserDashboard,
+  },
+  data() {
+    return {
+      isLoggedIn: false
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ portofolio-vue/src/components/UserLogin.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login">
+    <h2>Login</h2>
+    <form @submit.prevent="handleLogin">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" v-model="username" required />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" required />
+      </div>
+      <button type="submit">Login</button>
+      <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      errorMessage: '' 
+    };
+  },
+  methods: {
+    handleLogin() {
+      
+      if (this.username === 'admin' && this.password === 'admin') {
+        this.$emit('loginSuccess'); 
+      } else {
+        
+        this.errorMessage = "Invalid username or password";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.error {
+  color: red;
+}
+</style>
